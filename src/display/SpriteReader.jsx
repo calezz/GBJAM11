107b7027ead7971b027115eb02b55c7361347343
calezz <godzero89@gmail.com>
2023-09-13 14:39:15 +0100

    movement demo

--- src/display/SpriteReader.jsx
+++ src/display/SpriteReader.jsx
@@ -1,23 +1,46 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLogicStore } from "../store/Store";
 
 export default function SpriteReader() {
-    const store = useLogicStore((state)=>state.count)
-    
+  const [iso,setIso]=useState([0,0])
+
+  const store = useLogicStore((state) => state.count);
   const canvasRef = useRef(null);
- 
-  const [X,Y] = [16,16] //placeholder for cartesian 4 bit
-  const [isoX,isoY] = [X/2-Y/2,X/4+Y/4] //cartesian 4 bit to iso
-  const style={
-    transform:`translate(${isoX}px,${isoY}px)`
-}
-  const [WIDTH,HEIGHT] = [16,16] // asset size will be input here
-  const [DX,DY] = [0,0] // where the sprite will be drawn from relative to its own canvas 
-  
-  //TODO ASSET POSITION
-  useEffect(  () => {
-    const image =  new Image();
+  const [X, Y, Z] = [16, 16, -16]; //placeholder for cartesian 4 bit
+  const [isoX, isoY] = [X / 2 - Y / 2, X / 4 + Y / 4 + Z / 2]; //cartesian 4 bit to iso
+  const style = {
+    transform: `translate(${iso[0]}px,${iso[1]}px)`,
+  };
+  const [WIDTH, HEIGHT] = [16, 16]; // asset size will be input here
+  const [DX, DY] = [0, 0]; // where the sprite will be drawn from relative to its own canvas
+
+  function keyHandler(e){
+    if (e.key === "w") {
+      setIso([iso[0]+8,iso[1]-4]) 
+    }
+    if (e.key === "d") {
+        setIso([iso[0]+8,iso[1]+4]) 
+      }
+      if (e.key === "a") {
+        setIso([iso[0]-8,iso[1]-4]) 
+      }
+      if (e.key === "s") {
+        setIso([iso[0]-8,iso[1]+4]) 
+      }
+    
+    
+  }
+
+  useEffect(()=>{
+      window.addEventListener("keydown",keyHandler)
+      return ()=>{
+        window.removeEventListener("keydown",keyHandler)
+      }
+  },[iso])
+  useEffect(() => {
+    const image = new Image();
     image.src = "/placeholder2.png";
+    console.log("load")
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     const sourceX = 0;
@@ -31,25 +54,30 @@ export default function SpriteReader() {
     const destinationWidth = WIDTH;
     const destinationHeight = HEIGHT;
     ctx.imageSmoothingEnabled = false;
-
-    ctx.drawImage(
-      image,
-      sourceX,
-      sourceY,
-      sourceWidth,
-      sourceHeight,
-      destinationX + offsetX,
-      destinationY + offsetY,
-      destinationWidth,
-      destinationHeight
-    );
+    image.onload = ()=>{
+      ctx.drawImage(
+        image,
+        sourceX,
+        sourceY,
+        sourceWidth,
+        sourceHeight,
+        destinationX + offsetX,
+        destinationY + offsetY,
+        destinationWidth,
+        destinationHeight
+      );
+    }
     return () => ctx.clearRect(0, 0, 160, 144);
-  });
+  }, []);
 
   return (
-    <>
-      <p>{store}</p>
-      <canvas ref={canvasRef} style={style} height={`${HEIGHT}`} width={`${WIDTH}`} />
-    </>
+    <div>
+      <canvas
+        ref={canvasRef}
+        style={style}
+        height={`${HEIGHT}`}
+        width={`${WIDTH}`}
+      />
+    </div>
   );
 }
