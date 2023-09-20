faebbd53c071f107cbe8e24e58e9d115392b4299
calezz <godzero89@gmail.com>
2023-09-20 19:39:17 +0100

    Update LevelCamera.jsx

--- src/instances/LevelCamera.jsx
+++ src/instances/LevelCamera.jsx
@@ -13,27 +13,27 @@ const LevelCamera = memo(({children}) => {
       if (e.key === "w") {
       setPlayerState(93)
        movePlayer([0,-1,0])
-  //     moveCamera([-8,4])
+
       }
       if (e.key === "d") {
         movePlayer([1,0,0])
-    //    moveCamera([-8,-4]); 
+
       }
       if (e.key === "a") {
         movePlayer([-1,0,0])
-   //     moveCamera([+8,+4]); 
+
       }
       if (e.key === "s") {
         movePlayer([0,+1,0])
-     //   moveCamera([+8,-4])
+
       }
       if (e.key === " ") {
         movePlayer([0,0,1])
-  //      movePlayer([0,+8]);; 
+ 
       }
       if (e.key === "Control") {
         movePlayer([0,0,-1])
-    //    movePlayer([0,-8]); 
+
       }
     };
     //movement controller
